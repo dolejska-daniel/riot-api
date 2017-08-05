03250dcb69e6c9f9aa764b8daafec55756669a07
Daniel Dolejška <dolejskad@gmail.com>
2017-08-05 22:17:46 +0200

    LICENSE 2017 update

--- src/RiotAPI/Exceptions/SettingsException.php
+++ src/RiotAPI/Exceptions/SettingsException.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (C) 2016  Daniel Dolejška
+ * Copyright (C) 2016-2017  Daniel Dolejška
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
