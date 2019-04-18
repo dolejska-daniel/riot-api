df543413a58d3e84cfae1a699abe00a1bd7977a8
Daniel Dolejška <dolejskad@gmail.com>
2019-04-18 16:25:40 +0200

    License notice update 2019

--- src/LeagueAPI/Objects/ApiObjectLinkable.php
+++ src/LeagueAPI/Objects/ApiObjectLinkable.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (C) 2016-2018  Daniel Dolejška
+ * Copyright (C) 2016-2019  Daniel Dolejška
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
