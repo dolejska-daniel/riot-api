846723e741e7cf8bf7c756888fcf300866b97e6a
Daniel Dolejška <dolejskad@gmail.com>
2019-09-26 12:50:41 +0200

    Updated PHP version requirements for LeagueAPICLI

--- src/LeagueAPICLI/LeagueAPICLI.php
+++ src/LeagueAPICLI/LeagueAPICLI.php
@@ -29,8 +29,8 @@ foreach ($autoloader_paths as $autoloader_path)
 
 use RiotAPI\LeagueAPICLI\Application;
 
-if (PHP_VERSION_ID < 70000)
-	trigger_error("This version (" . PHP_VERSION . ") of PHP is not supported by this library. PHP7.0 or higher required.", E_USER_ERROR);
+if (PHP_VERSION_ID < 71000)
+	trigger_error("This version (" . PHP_VERSION . ") of PHP is not supported by this library. PHP7.1 or higher required.", E_USER_ERROR);
 
 if (!class_exists("RiotAPI\LeagueAPICLI\Application"))
 	trigger_error("Class autoloading failed. Library installation with Composer is required. Are you using Composer?", E_USER_ERROR);
